5d2a337865c5677211a5df31cd782f6408b22cd3
Wilson Maximo <wilson-max@hotmail.com>
2023-12-08 16:34:50 -0300

    Correction flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,12 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None or any(
-        period is None or len(period) != 2 or not all(isinstance(val, int) for val in period)
+        period is None or len(period) != 2 or not all(isinstance(val, int)
+                                                      for val in period)
+
         for period in permanence_period
     ):
         return None
 
-    count_of_periods = sum(start <= target_time <= end for start, end in permanence_period)
+    count_of_periods = sum(start <= target_time <=
+                           end for start, end in permanence_period)
     return count_of_periods
