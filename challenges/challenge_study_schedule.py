2cb1cd9ad675b07a32a343dd260823905b7a1cae
Wilson Maximo <wilson-max@hotmail.com>
2023-12-08 16:31:17 -0300

    Challenge one

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or any(
+        period is None or len(period) != 2 or not all(isinstance(val, int) for val in period)
+        for period in permanence_period
+    ):
+        return None
+
+    count_of_periods = sum(start <= target_time <= end for start, end in permanence_period)
+    return count_of_periods
