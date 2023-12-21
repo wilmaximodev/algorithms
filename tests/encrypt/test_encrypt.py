d691f18adc73ef98ca4ad1cb5998661d0ae92ec3
Wilson Maximo <wilson-max@hotmail.com>
2023-12-21 20:55:33 -0300

    Test correction

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,1 +1,16 @@
-# refazer teste
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+
+def test_encrypt_message():
+    with pytest.raises(TypeError):
+        encrypt_message("abcdef", "f") == "invalid type for key"
+
+    with pytest.raises(TypeError):
+        encrypt_message(10, "f") == "invalid type for message"
+
+    assert encrypt_message("trybe", 8) == "ebyrt"
+
+    assert encrypt_message("trybe", 3) == "yrt_eb"
+
+    assert encrypt_message("trybe", 2) == "eby_rt"
