592bdc1a7f5d08e7b5f290d43d368751f2f1db88
Wilson Maximo <wilson-max@hotmail.com>
2023-12-21 21:11:31 -0300

    Challenge five

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not all(isinstance(num, int) for num in nums):
+        return False
+
+    seen = set()
+    for num in nums:
+        if num < 0 or num > len(nums) - 1:
+            return False
+        if num in seen:
+            return num
+        seen.add(num)
+    return False
