4edb2ed521014402c22bf54be9b993cd61aa4386
Wilson Maximo <wilson-max@hotmail.com>
2023-12-21 21:21:01 -0300

    Challenge 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,20 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+      # Verifica se a palavra é vazia
+    if not word:
+        return False
+
+    # Inicializa os índices
+    start, end = 0, len(word) - 1
+
+    # Itera até que os índices se cruzem
+    while start < end:
+        # Compara os caracteres nas posições opostas
+        if word[start] != word[end]:
+            return False
+
+        # Move os índices para o centro
+        start += 1
+        end -= 1
+
+    # Se a verificação for bem-sucedida, é um palíndromo
+    return True
